5848a1b9754a7811f8d8c79ecc8a895db714890e
Wahiduz Zaman <156948092+Wahiduz89@users.noreply.github.com>
2025-04-04 22:05:14 +0530

    Add frontend/src/pages/Login.tsx 1743784511767

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold">Login</h1>
+      {/* Add login form here */}
+    </div>
+  );
+};
+
+export default Login;
