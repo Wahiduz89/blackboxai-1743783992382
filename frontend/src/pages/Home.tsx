4d37624823bf72932cf5dee3b1a473078641929c
Wahiduz Zaman <156948092+Wahiduz89@users.noreply.github.com>
2025-04-04 22:05:15 +0530

    Add frontend/src/pages/Home.tsx 1743784513172

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold">Welcome to the OTT Platform</h1>
+      {/* Add more content here */}
+    </div>
+  );
+};
+
+export default Home;
