122244450507e5e44a07e6e617afdd3245bbc76e
Wahiduz Zaman <156948092+Wahiduz89@users.noreply.github.com>
2025-04-04 22:05:27 +0530

    Add frontend/src/App.tsx 1743784524595

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import VideoDetails from './pages/VideoDetails';
+import NotFound from './pages/NotFound';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/videos/:id" element={<VideoDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
